99923bdc80c455290040c44d570b6960d13fd873
krankie <foresvives10@gmail.com>
2020-09-24 14:32:52 +0100

    boilerplate

--- public/examples/3d-obj-loader/scripts.js
+++ public/examples/3d-obj-loader/scripts.js
@@ -1,53 +1,69 @@
-var scene = new THREE.Scene();
+ // Create the Three.js Scene
+ var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 200;
+ // Create a new Perspective Camera
+ var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
+ camera.position.z = 25;
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+ // Create a Full Screen WebGL Renderer
+ var renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+ renderer.setClearColor("#DDDDDD");
+ renderer.setSize(window.innerWidth,window.innerHeight);
 
-var controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enableZoom = true;
+ document.body.appendChild(renderer.domElement);
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
-keyLight.position.set(-100, 0, 100);
+ // Make sure the project is responsive based on window resizing
+ window.addEventListener('resize', () => {
+     renderer.setSize(window.innerWidth,window.innerHeight);
+     camera.aspect = window.innerWidth / window.innerHeight;
 
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
-fillLight.position.set(100, 0, 100);
+     camera.updateProjectionMatrix();
+ })
 
-var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
-backLight.position.set(100, 0, -100).normalize();
+ // Add a light
+ var light = new THREE.PointLight(0xFFFFFF, 1.4, 1000)
+ light.position.set(0,15,15);
+ scene.add(light);
 
-scene.add(keyLight);
-scene.add(fillLight);
-scene.add(backLight);
+ // Defining a variable for our two models
+ var ourObj2;
 
-var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setTexturePath('/examples/3d-obj-loader/assets/');
-mtlLoader.setPath('/examples/3d-obj-loader/assets/');
-mtlLoader.load('r2-d2.mtl', function (materials) {
 
-    materials.preload();
 
-    var objLoader = new THREE.OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.setPath('/examples/3d-obj-loader/assets/');
-    objLoader.load('r2-d2.obj', function (object) {
+ // Create a material
+ var mtlLoader = new THREE.MTLLoader();
+ mtlLoader.load('./r2-d2.mtl', function (materials) {
 
-        scene.add(object);
-        object.position.y -= 60;
+     materials.preload();
 
-    });
+     // Load the object
+     var objLoader = new THREE.OBJLoader();
+     objLoader.setMaterials(materials);
+     objLoader.load('./r2-d2.obj', function (object) {
+         scene.add(object);
+         ourObj2 = object;
+         object.position.z -= 70;
+         object.rotation.x = 250;
 
-});
+         this.tl = new TimelineMax();
+         this.tl.from(ourObj2.scale, 2, {y: 0, x:0, z: 0, ease: Expo.easeOut})
+         this.tl.from(ourObj2.position, 2, {y: 0, z: -30,  ease: Expo.easeOut})
+         this.tl.from(ourObj.scale, 2, {x: 1.5, y: 1.5, z: 0, ease: Expo.easeOut}, '-=2')
+         this.tl.from(ourObj.position, 1, {y: -20, ease: Expo.easeOut}, '-=3')
+     });
+ });
+ 
 
-var animate = function () {
-	requestAnimationFrame( animate );
-	controls.update();
-	renderer.render(scene, camera);
-};
 
-animate();
+
+ var render = function() {
+     requestAnimationFrame(render);
+     
+     // Rotate the objects indefinitely
+    /*  ourObj2.rotation.z += .03; */
+
+     renderer.render(scene, camera);
+ }
+
+ // Call this to render the entire scene
+ render();
