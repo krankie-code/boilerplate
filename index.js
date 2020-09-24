1b132a27a32ab429baa40f1d19868fba69fcac20
krankie <foresvives10@gmail.com>
2020-09-24 15:35:04 +0100

    file updated

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ app.listen(app.get('port'), function(err) {
   } else {
     console.log('Running on port: ' + app.get('port')); }
 });
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+ res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
