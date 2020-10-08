5c7d80f79d3c047b0a65fda140306fefdad008ba
krankie <foresvives10@gmail.com>
2020-10-08 19:12:44 +0200

    3d model

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 200;
+
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
